525a9cd0788f4387b0255f446ee467d223b7d591
soulef77 <saoud.soulef@laposte.net>
2026-01-08 11:49:13 +0100

    Add Swagger documentation 10

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { json, urlencoded } from 'express';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     rawBody: true, // Important pour les webhooks Stripe
